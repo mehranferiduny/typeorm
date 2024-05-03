1a873f0961befa599e3a26a801b7ca5e515c7413
mehran feriduni <m3hr4nus@gmail.com>
2024-05-04 00:17:06 +0330

    add dto and qurey select

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  f_name:string;
+  @IsString()
+  @IsNotEmpty()
+  l_name:string;
+  @IsString()
+  @IsNotEmpty()
+  email:string;
+  @IsNumber()
+  @IsNotEmpty()
+  age:number;
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { ILike, LessThan, LessThanOrEqual, Like, MoreThan, MoreThanOrEqual, Not, Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
@@ -11,26 +11,26 @@ export class UserService {
     @InjectRepository(UserEntity) private userRepository:Repository<UserEntity>
   ){}
  async create(createUserDto: CreateUserDto) {
+  const {f_name,l_name,email,age}=createUserDto;
     const user= this.userRepository.create({
-      f_name:"ali",
-      l_name:"fereiduni",
-      email:"ali2424@gmail.com",
-      age:25
+    f_name,l_name,email,age
     })
     return await this.userRepository.save(user)
   }
  async insert(createUserDto: CreateUserDto) {
-
+  const {f_name,l_name,email,age}=createUserDto;
     return await this.userRepository.insert({
-      f_name:"hamed",
-      l_name:"hasani",
-      email:"hamedkhan@gmail.com",
-      age:26
+      f_name,l_name,email,age
     })
   }
 
   async findAll() {
-    return await this.userRepository.findBy({});
+    return await this.userRepository.find({
+      //! ILIKE Like
+      //! NOT
+      //! MoreThan  MoreThanOrEqual LessThan  LessThanOrEqual
+      where:{id:LessThanOrEqual(4)}
+    });
   }
 
   findOne(id: number) {
@@ -45,3 +45,4 @@ export class UserService {
     return `This action removes a #${id} user`;
   }
 }
+
